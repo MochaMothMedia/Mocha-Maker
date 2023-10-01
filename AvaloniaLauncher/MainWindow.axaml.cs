d2e8f260e6296dce3e392c30afe3505e4e017d6f
MochaMoth <MochaMothMedia@gmail.com>
2023-10-01 11:00:12 -0700

    Create initial project layout

---
+++ AvaloniaLauncher/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MochaMothMedia.MochaMaker.AvaloniaLauncher
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ AvaloniaLauncher/Program.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using System;
+
+namespace MochaMothMedia.MochaMaker.AvaloniaLauncher
+{
+	internal class Program
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			_ = BuildAvaloniaApp()
+				.StartWithClassicDesktopLifetime(args);
+		}
+
+		public static AppBuilder BuildAvaloniaApp()
+		{
+			return AppBuilder.Configure<App>()
+				  .UsePlatformDetect()
+				  .WithInterFont()
+				  .LogToTrace();
+		}
+	}
+}
